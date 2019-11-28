5df261e038ea9dd50566c2ca61f907151ca9e60a
longquan521 <569978412@qq.com>
2019-11-28 16:16:27 +0800

    init shop

---
+++ src/views/Category.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>种类</div>
+</template>
+
+<script>
+</script>
+<style lang="scss">
+</style>
---
+++ src/views/Profile.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>我的</div>
+</template>
+
+<script>
+</script>
+<style lang="scss">
+</style>
