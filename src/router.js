a17c653f85c1bca4747cc8cdd94571fa02d33ebd
longquan521 <54140828+longquan521@users.noreply.github.com>
2019-12-05 14:24:09 +0800

    Update router.js

--- src/router.js
+++ src/router.js
@@ -47,7 +47,7 @@ export default new Router({
         default: Cart,
         "footer-bar": FooterBar
       }, meta: {
-        keepAlive: true
+        keepAlive: false
       }
     }, {
       path: '/profile',
